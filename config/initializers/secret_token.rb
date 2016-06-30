6f96a9b34b57c08608ffeb97f2b3cdb972177c15
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-06-30 16:35:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mactodo::Application.config.secret_key_base = 'a86eea1d12471b4519fdcb18ffe949ca4da3944eaf0b0f33e8d6c2e6a3d349a3a342ea6ba1c448d02980d383b52431c24357e55972f0e07e5dbc96444d6ef0c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mactodo::Application.config.session_store :cookie_store, key: '_mactodo_session'
